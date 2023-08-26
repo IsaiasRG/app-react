dc523a57ade585c0fa8f1cae0b77d9c1e034861e
Isaias Ramirez :) <isaias.ramirez2004@gmail.com>
2023-08-26 16:39:18 -0300

    probando que aparezca el navegador

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {NavBar} from './components/NavBar/NavBar';
-import {Titulo} from './components/Titulo/Titulo';
+import NavBar from './components/NavBar/NavBar';
+import Titulo from './components/Titulo/Titulo';
 
 function App() {
   return (
